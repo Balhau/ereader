5f8e86cba61e3bbb5ef984ededf58d1ad75cadb2
Vitor Fernandes <vitor.fernandes@blip.pt>
2016-10-08 19:35:37 +0100

    Initial commit

---
+++ src/main/java/org/balhau/books/App.java
@@ -0,0 +1,13 @@
+package org.balhau.books;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args ) {
+
+
+    }
+}
---
+++ src/test/groovy/com/balhau/books/AppTest.groovy
@@ -0,0 +1,12 @@
+package com.balhau.books;
+
+class AppTest extends Specification {
+
+    def setup() {
+        println "Setup instance"
+    }
+
+    def "Dummy test"() {
+
+    }
+}
